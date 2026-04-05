ee9b8a1667e6e73b3c7e9f9639390a39341c9a61
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 22:22:59 +0200

    freeimage v3.19.10

---
+++ atom.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+###############################################################################
+#  lz4
+###############################################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := freeimage
+LOCAL_DESCRIPTION := FreeImage graphics library
+LOCAL_CATEGORY_PATH := libs
+LOCAL_ARCHIVE_VERSION := 3.19.10
+LOCAL_ARCHIVE := FreeImage-$(LOCAL_ARCHIVE_VERSION).tar.gz
+LOCAL_ARCHIVE_SUBDIR := FreeImage-$(LOCAL_ARCHIVE_VERSION)
+
+# LOCAL_EXPORT_LDLIBS := -llz4
+
+include $(BUILD_CMAKE)
